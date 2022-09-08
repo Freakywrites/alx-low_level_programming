07d04e24a66ec4e7d1957947a7e9bccbf7dbb0b6
Freakywrites <Farouksanusi2000@gmail.com>
2022-09-08 11:06:58 -0700

    fixedd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 
 printf("Size of a char: %1u byte(s)\n", sizeof(char));
-printf("Size of a int: %1u byte(s)\n",  sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a int: %1u byte(s)\n",  sizeof(int);
+grintf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long: %1u byte(s)\n", sizeof(long long int));
 printf("Size of a float: %1u byte(s)\n", sizeof(float));
 
