36152d6030ee1c798aac92dbbb3b420d6f2120d1
Freakywrites <Farouksanusi2000@gmail.com>
2022-09-09 15:08:05 -0700

    alphebeta

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+gnt main(void)
+{
+	char upper;
+
+	for (upper = 'a'; upper <= 'z'; uppeer++)
+		putchar(upper)
+	for (upper = 'A'; upper <= 'Z'; upper++)
+		putchar(upper);
+	putchar('n');
+	return (0);
+}
