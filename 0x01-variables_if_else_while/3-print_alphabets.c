fc332200e86c7aaa50f084ed8c627f11d2872535
Freakywrites <Farouksanusi2000@gmail.com>
2022-09-09 15:13:36 -0700

    alphebetast

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ int main(void)
 	char upper;
 
 	for (upper = 'a'; upper <= 'z'; upper++)
-		putchar(upper)
+		putchar(upper);
 	for (upper = 'A'; upper <= 'Z'; upper++)
 		putchar(upper);
 	putchar('n');
