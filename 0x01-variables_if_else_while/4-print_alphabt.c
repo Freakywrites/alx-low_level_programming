20cc7ae78905f05a2b35012ff5d8c965ca5c6a57
Freakywrites <Farouksanusi2000@gmail.com>
2022-09-09 15:27:11 -0700

    alphebetass2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		if (alph != e && alp != 0)
+			putchar(alp)
+	}
+}
