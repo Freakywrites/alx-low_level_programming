23bf9f0be40ed5731694c6e95c907bea737a5edc
Freakywrites <Farouksanusi2000@gmail.com>
2022-09-13 06:39:41 -0700

    checks for an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - alphabetic character
+ *
+ * @c: this is used in comparison
+ * Return: Always 0 (Success)
+ **/
+int _isalpha(int c)
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
