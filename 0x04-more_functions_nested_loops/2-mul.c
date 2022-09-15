2ba6fb01ad4e68fec80f7db54b7b4499afc20617
Freakywrites <Farouksanusi2000@gmail.com>
2022-09-15 06:09:55 -0700

    d

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point for the code
+ *
+ * Return: Always 0.
+ **/
+
+int main (void)
+{
+	int mul(int a, int b)
+	{
+		mul = a * b;
+
+		return mul;
+	}
+}
