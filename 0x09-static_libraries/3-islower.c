4417788edf244a61989f520457638a78ee0752a5
Freakywrites <Farouksanusi2000@gmail.com>
2022-09-30 13:22:22 -0700

    static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase
+ * @c: it contains values that is being compared
+ * Return: Always 0 (Success)
+ **/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
